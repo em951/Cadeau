d0c72713fe03cef6af1949dcd8bf96b7be323bd5
Roihmatoun <roihmatoun5@gmail.com>
2024-03-27 12:26:12 +0400

    oula

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    accueil: './Cadeau/accueil.html', // Page principale
-    swap: './Cadeau/swap.html', // Autre page
-    main: './Cadeau/js/swap.js', // Fichier JavaScript principal
-    styles: './Cadeau/assets/css/swap.css', // Fichier CSS principal
+    accueil: './accueil.html', // Page principale
+    swap: './swap.html', // Autre page
+    main: './js/swap.js', // Fichier JavaScript principal
+    styles: './assets/css/swap.css', // Fichier CSS principal
   },
   output: {
-    path: path.resolve(__dirname, 'Cadeau/dist'), // Répertoire de sortie principal
+    path: path.resolve(__dirname, 'dist'), // Répertoire de sortie principal
     filename: '[name]/js/[name].bundle.js', // Chemin de sortie pour les fichiers JavaScript
   },
   module: {
